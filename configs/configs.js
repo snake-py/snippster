f10a9b94825d653131f10302e50cd97cb7be935d
snake-py <vabischannel@gmail.com>
2021-11-28 12:47:01 +0100

    changed configs

--- configs/configs.js
+++ configs/configs.js
@@ -1,5 +1,5 @@
 // ENV Variables
 // ----------------
-// process.env.DEV_APP_PATH='C:/Users/vabis/Desktop/Playground/projects/snippster-devData'
-process.env.IS_PUBLISHED='True'
+process.env.DEV_APP_PATH='C:/Users/vabis/Desktop/Playground/projects/snippster-devData'
+process.env.IS_PUBLISHED=''
 // ----------------
